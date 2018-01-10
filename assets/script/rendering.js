7be7eff349978722f28846d8367829f5d26ab0d3
András Kamocsai <kamo.andras@gmail.com>
2018-01-10 14:17:38 +0100

    feat(rendering.js): added createTable function

--- assets/script/rendering.js
+++ assets/script/rendering.js
@@ -1,2 +1,13 @@
 'use strict'
 
+function renderCards( cardCode ) {
+  let card = 
+  cardCode.forEach(function (code) {
+    code.split('1' || '2' || '3');
+  });
+};
+
+function renderTable( card ) {
+  let table = document.querySelector('.game-table');
+  
+};
