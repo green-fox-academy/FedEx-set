e17fad037a373e395c014f2550edf7ab3fd7a3b8
Balazs Prjevara <prjevarabalazs@gmail.com>
2018-01-10 16:12:55 +0100

    feat(game-logic): create set variable to count correct matches

--- assets/script/game_logic.js
+++ assets/script/game_logic.js
@@ -1,8 +1,11 @@
+'use strict';
+let set = 0;
+
 let firstNumber = '1'; 
 let secondNumber = '2';
 let thirdNumber = '3';
 
-let firstShape = 'A';
+let firstShape = 'B';
 let secondShape = 'B';
 let thirdShape = 'C';
 
@@ -11,38 +14,38 @@ let secondPattern = 'A';
 let thirdPattern = 'B';
 
 let firstColor = 'A';
-let secondColor = 'A';
+let secondColor = 'B';
 let thirdColor = 'A';
 
 function cardChecker(x,y,z){
     if (numberChecker(x,y,z)){
-        if (attributeChecker(firstShape, secondShape, thirdShape)){
-            if (attributeChecker(firstPattern, secondPattern, thirdPattern)){
-                if (attributeChecker(firstColor, secondColor, thirdColor)){
-                    console.log('set')
+        if (attributeChecker(firstShape, secondShape, thirdShape, 'shape')){
+            if (attributeChecker(firstPattern, secondPattern, thirdPattern, 'pattern')){
+                if (attributeChecker(firstColor, secondColor, thirdColor, 'color')){
+                    console.log(set)
                     // return true
-                } else{
-                    console.log('wrong color');
-                }
-            } else{
-                console.log('wrong pattern');
+                } 
             }
-        } else{
-            console.log('wrong shape');
         }
     }
 };
 
 function numberChecker(x, y, z){
     if (x === y && y === z || Number(x) + Number(y) + Number(z) === 6){
+        set ++;
         return true
-    }else{
+    }else {
         console.log('wrong number');
+        return true
     }
 };
 
-function attributeChecker(x, y, z){
+function attributeChecker(x, y, z, atribute){
     if (x === y && y === z || x !== y && y !== z && x !== z){
+        set ++;
+        return true
+    } else {
+        console.log('Wrong ' + atribute)
         return true
     }
 };
