fb6e88709dd6b0681ea94b29e98c3ef719c13ed5
AHolcsik <annamaria.holcsik@gmail.com>
2018-01-10 20:31:49 +0100

    fix(rendering): cards rendered with the same image

--- assets/script/rendering.js
+++ assets/script/rendering.js
@@ -5,16 +5,14 @@ function renderCards(cardCode) {
     cardCode.forEach(function (code) {
     let patternNumber = code[0]
     let i = 0
-    // let patternNumber = code.length[0]
     let pattern = code.substr(code.length -3)
     // console.log(pattern)
-    let cardContent = `<div class="card" id="1"></div>`
-    let cardPattern = `<img src='../images/${pattern}.png' alt>`
+    let cardContent = `<div class="content"><img src="assets/images/ceb.png" alt=""></div>`
+    let cardPattern = `<img src='../images/${pattern}.png' alt>`.repeat(patternNumber)
     let cardElement = document.createElement('div')
     cardElement.setAttribute('class', 'card')
     cardElement.setAttribute('id', i)
-    // `<div class="card" id="${i}">${cardPattern}</div>`
-    // cardElement.innerHTML = cardPattern
+    cardElement.innerHTML = cardContent
     let tableContainer = document.querySelectorAll('.card-container')[0];
     tableContainer.appendChild(cardElement)
     i++
@@ -61,7 +59,6 @@ let createCards = function(callback) {
             cards[l] = 3 + cards[l]
         }
     }
-    console.log(cards)
     callback(cards)
 }
 createCards(renderCards)
