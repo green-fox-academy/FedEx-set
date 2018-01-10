77d789ab52a11347a7632407a6c9b4cfd48b4b56
Balazs Prjevara <prjevarabalazs@gmail.com>
2018-01-10 14:23:06 +0100

    feat(game-logic): create cardChecker function

--- assets/script/game_logic.js
+++ assets/script/game_logic.js
@@ -1,37 +1,50 @@
-let firstNumber
-let secondNumber
-let thirdNumber
+let firstNumber = '1'; 
+let secondNumber = '2';
+let thirdNumber = '3';
 
-let firstShape
-let secondShape
-let thirdShape
+let firstShape = 'A';
+let secondShape = 'B';
+let thirdShape = 'C';
 
-let firstPattern
-let secondPattern
-let thirdPattern
+let firstPattern = 'B';
+let secondPattern = 'A';
+let thirdPattern = 'B';
 
-let firstColor
-let secondColor
-let thirdColor
+let firstColor = 'A';
+let secondColor = 'A';
+let thirdColor = 'A';
 
-function cardChecker(){
-    pass
-}
-
-function numberChecker(x, y, z){
-    if (x === y && y === z || Number(x) + Number(y) + Number(z) === 6) {
-        return true;
-    };
+function cardChecker(x,y,z){
+    if (numberChecker(x,y,z)){
+        if (attributeChecker(firstShape, secondShape, thirdShape)){
+            if (attributeChecker(firstPattern, secondPattern, thirdPattern)){
+                if (attributeChecker(firstColor, secondColor, thirdColor)){
+                    console.log('set')
+                    // return true
+                } else{
+                    console.log('wrong color');
+                }
+            } else{
+                console.log('wrong pattern');
+            }
+        } else{
+            console.log('wrong shape');
+        }
+    }
 };
 
-function shapeChecker(){
-    pass
-}
+function numberChecker(x, y, z){
+    if (x === y && y === z || Number(x) + Number(y) + Number(z) === 6){
+        return true
+    }else{
+        console.log('wrong number');
+    }
+};
 
-function patternChecker(){
-    pass
-}
+function attributeChecker(x, y, z){
+    if (x === y && y === z || x !== y && y !== z && x !== z){
+        return true
+    }
+};
 
-function colorChecker(){
-    pass
-}
+cardChecker(firstNumber, secondNumber, thirdNumber);
