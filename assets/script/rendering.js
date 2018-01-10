0111fefb5ed4f3824fb1853f50b2d64a885f3d3a
AHolcsik <annamaria.holcsik@gmail.com>
2018-01-10 14:31:09 +0100

    feat(rendering): deck of cards created

--- assets/script/rendering.js
+++ assets/script/rendering.js
@@ -1,2 +1,20 @@
 'use strict'
 
+let cards = []
+
+let createCards = function() {
+    let shapes = ['d', 'w', 'c']
+    let pattern = ['e', 'f', 's']
+    let color = ['r', 'g', 'b']
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++){
+            for (let l = 0; l < 3; l++){
+                let card = i + shapes[i] + pattern[j] + color[l]
+                cards.push(card)
+            }
+        }
+    }
+    return cards
+}
+
+createCards()
