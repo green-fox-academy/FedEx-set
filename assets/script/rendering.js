a9913ef8121cb4c7bd3b93ee68404a9c5cc6ab0c
AHolcsik <annamaria.holcsik@gmail.com>
2018-01-10 20:13:20 +0100

    fix(rendering): rendering happens, content is off

--- assets/script/rendering.js
+++ assets/script/rendering.js
@@ -2,23 +2,23 @@
 
 function renderCards(cardCode) {
   console.log(cardCode)
-  cardCode.forEach(function (code) {
-    let i = 0
+    cardCode.forEach(function (code) {
     let patternNumber = code[0]
-    // console.log(patternNumber)
+    let i = 0
+    // let patternNumber = code.length[0]
     let pattern = code.substr(code.length -3)
     // console.log(pattern)
+    let cardContent = `<div class="card" id="1"></div>`
     let cardPattern = `<img src='../images/${pattern}.png' alt>`
-    let cardContent = `class="card" id="${i}"`
-    ${cardPattern.repeat(patternNumber)}
     let cardElement = document.createElement('div')
-    cardElement.innerHTML = cardContent
+    cardElement.setAttribute('class', 'card')
+    cardElement.setAttribute('id', i)
+    // `<div class="card" id="${i}">${cardPattern}</div>`
+    // cardElement.innerHTML = cardPattern
+    let tableContainer = document.querySelectorAll('.card-container')[0];
+    tableContainer.appendChild(cardElement)
     i++
     // renderTable(cardElement)
-    let tableContainer = document.querySelectorAll('.card-container')[0];
-    // console.log(renderedCard)
-    tableContainer.appendChild(cardElement)
-
   });
 };
 
